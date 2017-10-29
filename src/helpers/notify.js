e05ff246ee99fcc5dca5783dba49aac6a3eb2926
TJ Draper <tj@buzzingpixel.com>
2017-10-29 00:53:30 -0500

    Add notification icons

--- src/helpers/notify.js
+++ src/helpers/notify.js
@@ -1,13 +1,16 @@
 FAB.notify = function(message, isError) {
     var notifier = require('node-notifier');
     var title = 'Fabricator';
+    var icon = __dirname + '/../icons/fab-success-icon.png';
 
     if (isError) {
         title += ' Error';
+        icon = __dirname + '/../icons/fab-error-icon.png';
     }
 
     notifier.notify({
         title: title,
-        message: message
+        message: message,
+        icon: icon
     });
 };
