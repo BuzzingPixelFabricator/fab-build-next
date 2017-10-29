0e2dff515ce593ec357945bf790fa87a91133fff
TJ Draper <tj@buzzingpixel.com>
2017-10-28 21:14:36 -0500

    Add initial CSS build

---
+++ src/css.js
@@ -0,0 +1,67 @@
+/*----------------------------------------------------------------------------*\
+    # Copyright 2017, BuzzingPixel, LLC
+
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the Apache License 2.0.
+    # http://www.apache.org/licenses/LICENSE-2.0
+
+    # This file is an example and not required
+\*----------------------------------------------------------------------------*/
+
+// Get Node requirements
+var recursive = require('recursive-readdir-sync');
+var path = require('path');
+var postcss = require('postcss');
+var postcssNext = require('postcss-cssnext');
+var CleanCSS = require('clean-css');
+
+// Set up variables
+var fabCacheDirectory = global.projectRoot + '/fabCache';
+var fabCacheCssDirectory = fabCacheDirectory + '/css';
+var fabCacheCssBundleFile = fabCacheCssDirectory + '/bundle.css';
+var cssLoc = global.projectRoot + '/' + FAB.config.source + '/css';
+var customReset = cssLoc + '/reset.css';
+var bundleContents = '';
+var cssOutputDir = global.projectRoot + '/' + FAB.config.assets + '/css';
+var cssOutput = cssOutputDir + '/style.min.css';
+var outputDirPath = '';
+
+// Create the output directory
+cssOutputDir.split('/').forEach(function(path) {
+    if (! path) {
+        return;
+    }
+    outputDirPath += '/' + path;
+    FAB.mkdirIfNotExists(outputDirPath);
+});
+
+// Create the fabCache directories
+FAB.mkdirIfNotExists(fabCacheDirectory);
+FAB.mkdirIfNotExists(fabCacheCssDirectory);
+
+// Create the bundled CSS file
+FAB.writeFile(fabCacheCssBundleFile);
+
+// Start with the reset if it exists
+if (FAB.fileExists(customReset)) {
+    FAB.writeFile(fabCacheCssBundleFile, FAB.readFile(customReset));
+}
+
+// Add all other CSS files
+recursive(cssLoc).forEach(function(file) {
+    if (path.extname(file) !== '.css' || file === customReset) {
+        return;
+    }
+    FAB.writeFile(fabCacheCssBundleFile, FAB.readFile(file), true);
+});
+
+// Minify the concatenated file
+bundleContents= new CleanCSS()
+    .minify(FAB.readFile(fabCacheCssBundleFile).toString())
+    .styles;
+
+// Process CSS with postcss
+postcss([postcssNext]).process(bundleContents).then(function(result) {
+    // Write the output to the min file
+    FAB.writeFile(cssOutput, result.css);
+});
---
+++ src/fab.js
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------------*\
+    # Copyright 2017, BuzzingPixel, LLC
+
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the Apache License 2.0.
+    # http://www.apache.org/licenses/LICENSE-2.0
+
+    # This file is an example and not required
+\*----------------------------------------------------------------------------*/
+
+var path = require('path');
+
+// Set fab
+FAB = {};
+
+// Set the base config
+require('./config.js');
+
+// Require helpers
+require('fs').readdirSync(__dirname + '/helpers').forEach(function(file) {
+    if (path.extname(file) !== '.js') {
+        return;
+    }
+    require('./helpers/' + file);
+});
+
+// Run CSS
+if (FAB.config.enableCss) {
+    require('./css.js');
+}
