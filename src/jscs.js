f284f7f851492055ab4c7d093481821988cc7397
TJ Draper <tj@buzzingpixel.com>
2017-11-07 15:34:04 -0500

    Fix copyright comment headers

--- src/jscs.js
+++ src/jscs.js
@@ -4,8 +4,6 @@
     # This program is free software: you can redistribute it and/or modify
     # it under the terms of the Apache License 2.0.
     # http://www.apache.org/licenses/LICENSE-2.0
-
-    # This file is an example and not required
 \*----------------------------------------------------------------------------*/
 
 /* global FAB:true */
